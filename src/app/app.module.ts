08934cf16928905dbba8c2ad26a0514aa61781bc
Manikandan <Manikandan.P@expeditors.com>
2021-10-14 20:02:56 +0530

    FRONT-END CODE-updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { LoginComponent } from './login/login.component';
 import { ProductComponent } from './product/product.component';
 import { UserserviceService } from './userservice.service';
+import {AppRoutingModule} from "./app-routing.module";
+
 
 @NgModule({
   declarations: [
